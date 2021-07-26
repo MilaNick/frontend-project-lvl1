92aaa669763dae1ecf9c8472c0f94a8ac0b9071f
Mila Zolotova <l.zolotova@yahoo.com>
2021-07-27 02:38:33 +0300

    пробелы добавлены в строку 24 ))))

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -9,7 +9,7 @@ const getLargestDivisor = (a, b) => (b === 0 ? a : getLargestDivisor(b, a % b));
 export const gcdRound = () => {
   const num1 = tools.random(100);
   const num2 = tools.random(100);
-  engine.outputTerminal(`Question:${num1}${num2}`);
+  engine.outputTerminal(`Question: ${num1} ${num2}`);
   const answer = +readlineSync.question('Your answer: ');
   const correctAnswer = getLargestDivisor(num1, num2).toString();
   engine.choice(answer, correctAnswer);
