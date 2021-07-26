21f40a7b6fd2e6bfc6876ff6e3a0c312c5ea46ac
Mila Zolotova <l.zolotova@yahoo.com>
2021-07-27 02:11:56 +0300

    проверка - изменение файла calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -21,7 +21,8 @@ export const calcRound = () => {
   const num1 = tools.random(10);
   const num2 = tools.random(10);
   const operator = getRandomOperator();
-  engine.outputTerminal(`Question: ${num1} ${operator} ${num2}`);
+  console.log(`Question: ${num1} ${operator} ${num2}`);
+  // engine.outputTerminal(`Question: ${num1} ${operator} ${num2}`);
   const correctAnswer = getExpression(num1, num2, operator).toString();
   const answer = readlineSync.question('Your answer: ');
   engine.choice(answer, correctAnswer);
