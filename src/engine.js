850635b30cdb3224c8bccf01aa0a5f7cab12321f
Mila Zolotova <l.zolotova@yahoo.com>
2021-07-29 12:51:43 +0300

    правки по 3-ей проверке

--- src/engine.js
+++ src/engine.js
@@ -5,14 +5,13 @@ const engine = (desc, game, roundCounter) => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log(desc);
-  for (let i = 0; i < roundCounter;) {
+  for (let i = 0; i < roundCounter; i += 1) {
     const roundData = game();
     const { correctAnswer, question } = roundData;
     console.log(`Question: ${question}`);
     const answer = readlineSync.question('Your answer: ');
     if (answer === correctAnswer) {
       console.log('Correct!');
-      i += 1;
     } else {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
