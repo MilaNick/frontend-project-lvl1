f0678dc9d273f8482be7d9ffc1be248f92e6a4bd
Mila Zolotova <l.zolotova@yahoo.com>
2021-07-30 13:16:32 +0300

    изменение параметров для функции getRandomNumber

--- src/games/progression.js
+++ src/games/progression.js
@@ -20,7 +20,7 @@ const getRoundData = () => {
   const randomNum = getRandomNumber(0, 10);
   const step = getRandomNumber(0, 10);
   const count = getRandomNumber(5, 10);
-  const randomPosition = getRandomNumber(count, count - 1);
+  const randomPosition = getRandomNumber(0, count);
   const progression = getProgression(randomNum, step, count);
   const correctAnswer = progression[randomPosition].toString();
   const question = hideElem(progression, randomPosition);
