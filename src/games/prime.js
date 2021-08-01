c818726b4258c19795e4e951727d7123ef2a5583
Mila Zolotova <l.zolotova@yahoo.com>
2021-08-01 19:47:32 +0300

    правка prime.js, удаление условия Math.sqrt(elem) !== i

--- src/games/prime.js
+++ src/games/prime.js
@@ -7,7 +7,7 @@ const description = 'Answer "yes" if given number is prime. Otherwise answer "no
 const isPrime = (elem) => {
   if (elem <= 1) return false;
   for (let i = 2; i < Math.sqrt(elem); i += 1) {
-    if (Math.sqrt(elem) !== i && elem % i === 0) {
+    if (elem % i === 0) {
       return false;
     }
   }
