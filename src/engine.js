021bb7dfce749f4fc76f9d958c99d0c8f57e7223
Mila Zolotova <l.zolotova@yahoo.com>
2021-08-02 15:34:11 +0300

    замена параметра функции с game на run

--- src/engine.js
+++ src/engine.js
@@ -2,13 +2,13 @@ import readlineSync from 'readline-sync';
 
 const roundCounter = 3;
 
-const engine = (description, game) => {
+const engine = (description, run) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log(description);
   for (let i = 0; i < roundCounter; i += 1) {
-    const roundData = game();
+    const roundData = run();
     const { correctAnswer, question } = roundData;
     console.log(`Question: ${question}`);
     const answer = readlineSync.question('Your answer: ');
